d7450441c14700b8c43c71bf3ecf28acd551522f
gustavo <gustavoborgges98@hotmail.com>
2020-12-26 23:15:51 -0300

    middlawares

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,34 @@
 const express = require ('express')
-const { uuid } = require('uuidv4')
+const { uuid, isUuid } = require('uuidv4')
 
 const app = express()
 
 app.use(express.json())
 
 const projects = []
+
+function logRequests(request, response, next) {
+  const { method, url } = request;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.log(logLabel);
+
+  return next();
+}
+
+function validateProjectId(request, response, next) {
+  const { id } = request.params
+
+  if(!uuid(id)) {
+    return response.status(400).json({ error: 'Invalid project ID.' })
+  }
+
+  return next();
+}
+
+app.use(logRequests)
+app.use('/projects/:id', validateProjectId)
 
 app.get('/projects', (request, response) => {
   const { title } = request.query
@@ -15,6 +38,7 @@ app.get('/projects', (request, response) => {
     : projects
 
   return response.json(results);
+
 })
 
 app.post('/projects', (request, response) => {
@@ -26,8 +50,7 @@ app.post('/projects', (request, response) => {
   return response.json(project)
 })
 
-
-app.put('/projects/:id', (request, response) => {
+app.put('/projects/:id', validateProjectId, (request, response) => {
   const { id } = request.params
   const { title, owner } = request.body
 
@@ -48,11 +71,11 @@ app.put('/projects/:id', (request, response) => {
   return response.json(project)
 })
 
-app.delete('/projects/:id', (request, response) => {
+app.delete('/projects/:id', validateProjectId, (request, response) => {
   const { id } = request.params
 
   const projectIndex = projects.findIndex(project => project.id === id)
-
+ 
   if(projectIndex < 0) {
     return response.status(400).json({ error: 'Project not found..' })
   }
